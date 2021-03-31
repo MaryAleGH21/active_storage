682393f66b0065f3cd37977592b6336cdc14f517
Alejandra <marialesr.17@gmail.com>
2021-03-31 12:36:10 -0600

    modified controller products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,7 +65,7 @@ class ProductsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
-      @product = Product.find(params[:id])
+      @product = Product.with_attached_images.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
